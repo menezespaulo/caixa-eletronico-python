5927966c25bd7dada0b1be1381d0aaea886165bd
menezespaulo <menezespaulo1@hotmail.com>
2026-06-09 07:38:49 -0300

    Atualizando o sistema para usar dicionarios

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -1,12 +1,15 @@
-conta_correta = "1234"
-senha_correta = "5678"
-saldo_inicial = 1000.00 
+conta_usuario = {
+    "numero": "1234",
+    "senha": "5678",
+    "saldo_inicial": 1000.00    
+
+}
 
 print("Bem vindo ao sistema de autenticação do caixa eletrônico!")
 while True:
     conta = input("Digite o número da conta: ")
     senha = input("Digite a senha: ")
-    if conta == conta_correta and senha == senha_correta:
+    if conta == conta_usuario["numero"] and senha == conta_usuario["senha"]:
         print("Login bem-sucedido!")
         break
     else:
@@ -22,19 +25,19 @@ while True:
     opcao = input("Digite o número da operação desejada: ")
     match opcao:
         case "1":
-            print(f"Saldo em conta: R${saldo_inicial:.2f}")
+            print(f"Saldo em conta: R${conta_usuario["saldo_inicial"]:.2f}")
         case "2":
             valor_deposito = float(input("Insira o valor a ser depositado: "))
             if valor_deposito > 0:
-                saldo_inicial += valor_deposito 
-                print(f"Seu saldo atualizado é R${saldo_inicial:.2f}")
+                conta_usuario["saldo_inicial"] += valor_deposito 
+                print(f"Seu saldo atualizado é R${conta_usuario["saldo_inicial"]:.2f}")
         case "3":
             valor_saque = float(input("Insira o valor que deseja sacar: "))
-            if valor_saque > 0 and valor_saque <= saldo_inicial:
-                saldo_inicial -= valor_saque
+            if valor_saque > 0 and valor_saque <= conta_usuario["saldo_inicial"]:
+                conta_usuario["saldo_inicial"] -= valor_saque
                 print(f"Saque de R${valor_saque:.2f} realizado com sucesso!")
-                print(f"Seu saldo atualizado é de R${saldo_inicial:.2f}")
-            elif valor_saque > saldo_inicial:
+                print(f"Seu saldo atualizado é de R${conta_usuario["saldo_inicial"]:.2f}")
+            elif valor_saque > conta_usuario["saldo_inicial"]:
              print("Operação cancelada: Saldo insuficiente.")
             else:
                 print("Valor de saque inválido.")
